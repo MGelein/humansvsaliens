92bd8a19712b1e7e0e50645fb8bd799163512807
Jeroen Offerijns <jeroen@offerijns.nl>
2019-06-20 12:06:46 +0200

    fix: detection boundaries bug in tracking code

--- tracking/combined/webcam.py
+++ tracking/combined/webcam.py
@@ -28,14 +28,14 @@ from ssd import build_ssd
 # Config
 webcam_index = 0 # 0 for built-in webcam, 1 for external webcam (generally)
 max_face_size = 220 # based on the actual_face_size of people very close to the camera
-max_x = 640 # basically the webcam frame width
+max_x = 850 # basically the webcam frame width
 max_y = 200 # max distance at which we detect people (based on the actual_face_size)
 
 DEPTH_MODEL_NAME = 'mono_1024x320'
 
 print_fps = True # print FPS to stdout
 show_webcam = True
-show_map = False # show a map of the people in window while running
+show_map = True # show a map of the people in window while running
 map_width = 400
 map_height = 400
 
@@ -50,9 +50,8 @@ def get_bounding_boxes(frame):
     y = net(x)  # forward pass
     return y.data
 
-def get_depth_map(frame, feed_width, feed_height):
-    input_image = cv2.resize(frame, (feed_width, feed_height))
-    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+def get_depth_map(frame):
+    input_image = transforms.ToTensor()(frame).unsqueeze(0)
 
     # PREDICTION
     input_image = input_image.to(device)
@@ -115,16 +114,15 @@ with torch.no_grad():
 
         t1 = time.time()
 
-        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        input_image = cv2.resize(frame, (feed_width, feed_height))
 
-        # bboxes = face_detector.predict(rgb_frame, thresh)
-        bboxes = get_bounding_boxes(frame)
-        depth_map = get_depth_map(frame, feed_width, feed_height)
+        bboxes = get_bounding_boxes(input_image)
+        depth_map = get_depth_map(input_image)
 
         bg = np.zeros((map_height, map_width, 3))
 
         # scale each detection back up to the image
-        height, width = frame.shape[:2]
+        height, width = input_image.shape[:2]
         scale = torch.Tensor([width, height, width, height])
 
         # 15 is the index of the person class in the VOC label map
@@ -136,28 +134,24 @@ with torch.no_grad():
             data[j] = pt
 
             #requests.put("http://localhost:3000/people", data=data)
+            distance = depth_map[int(pt[0])][int(pt[j])] # closest faces should be lowest values
 
-            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 128, 0), 1)
-            cv2.putText(frame, str(depth_map[int(pt[0])][int(pt[j])]), (int(pt[0]), int(pt[1])), FONT, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.rectangle(input_image, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 128, 0), 1)
+            cv2.putText(input_image, str(distance), (int(pt[0]), int(pt[1])), FONT, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+            x = pt[0] / max_x # scale to 0-1
+            y = 1.0 - distance # scale to 0-1
+
+            cv2.circle(bg, (int(map_width - (map_width * x)), int((map_height - map_height * y))), 8, (255, 255, 255), -1)
+
             j += 1
-            
-        # for box in bboxes:
-        #     actual_face_size = math.sqrt(box[2] * box[3]) # use area of the face size as a measure of distance
-        #     distance = max_face_size - actual_face_size # closest faces should be lowest values
-
-        #     x = box[0] / max_x # scale to 0-1
-        #     y = distance / max_y # scale to 0-1
-        #     # print(str(x) + ", " + str(y))
-
-        #     cv2.circle(bg, (int(map_width - (map_width * x)), int((map_height - map_height * y))), 8, (255, 255, 255), -1)
-
         if show_map:
             cv2.imshow('map', bg)
 
         if show_webcam:
             # ann_frame = annotate_image(frame, bboxes)
-            frame = cv2.resize(frame, (640, 480)) / 255.
-            cv2.imshow('annotated_webcam', frame)
+            input_image = cv2.resize(input_image, (640, 480)) / 255.
+            cv2.imshow('annotated_webcam', input_image)
 
         if print_fps:
             fps = (fps + (1. / (time.time() - t1))) / 2
